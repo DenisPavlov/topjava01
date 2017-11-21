071553d163c672162801ccdd870643ef97e0fe67
DenisPavlov <denispavlov1989@gmail.com>
2017-11-21 12:18:54 +0700

    HW 03. Point 2. add meals db SQL script

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +10,11 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (datetime, description, calories, user_id) VALUES
+  ('2017-07-01 08:00:00', 'Завтрак user', '300', 100000),
+  ('2017-07-01 12:30:00', 'Обед user', '1700', 100000),
+  ('2017-07-01 18:00:00', 'Ужин user', '1500', 100000),
+  ('2017-07-02 08:00:00', 'Звтрак admin', '1500', 100001),
+  ('2017-07-02 12:00:00', 'Обед admin', '1500', 100001),
+  ('2017-07-02 18:00:00', 'Ужин admin', '1500', 100001)
