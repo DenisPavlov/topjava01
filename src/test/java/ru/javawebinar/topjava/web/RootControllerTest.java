fa88cc3a8df0371ac691c1fae958f063fdda8570
DenisPavlov <denispavlov1989@gmail.com>
2017-12-24 19:57:19 +0700

    RootControllerTest.testMeals для meals.jsp

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -6,10 +6,28 @@ import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.USER;
 import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 
 public class RootControllerTest extends AbstractControllerTest {
+    @Test
+    public void meals() throws Exception {
+        mockMvc.perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals", hasSize(MEALS.size())))
+                .andExpect(model().attribute("meals",contains(
+                        hasProperty("id", is(MEAL6.getId())),
+                        hasProperty("id", is(MEAL5.getId())),
+                        hasProperty("id", is(MEAL4.getId())),
+                        hasProperty("id", is(MEAL3.getId())),
+                        hasProperty("id", is(MEAL2.getId())),
+                        hasProperty("id", is(MEAL1.getId()))
+                )));
+    }
 
     @Test
     public void testUsers() throws Exception {
